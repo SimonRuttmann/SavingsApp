ff294db7c3735d08d1e775e3e498f3d915e8a049
Ulrich Michael <ulrich1999@web.de>
2022-04-05 14:23:37 +0200

    CDC-001: Added basic java server with spring boot
    Added java server with spring boot project for contentService, inflationService, userService

---
+++ backend/inflationService/src/main/java/service/inflationservice/InflationServiceApplication.java
@@ -0,0 +1,13 @@
+package service.inflationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflationServiceApplication.class, args);
+    }
+
+}
