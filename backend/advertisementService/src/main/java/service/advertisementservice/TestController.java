f80a9141d7eb29f5c4cddb16f49a5098185f6030
Ulrich Michael <ulrich1999@web.de>
2022-04-10 23:15:22 +0200

    #6 Adjusted all service poms and dockerfiles
    Every service should be installable with maven and runnable with docker

--- backend/advertisementService/src/main/java/service/advertisementservice/TestController.java
+++ backend/advertisementService/src/main/java/service/advertisementservice/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String index(){
-            return "Hallo meine angepasste Welt";
+            return "Hallo meine angepasste Welt des AdServices";
     }
 
 }
