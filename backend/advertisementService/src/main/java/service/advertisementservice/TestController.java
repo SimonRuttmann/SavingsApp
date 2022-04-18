1b6f14b2a53af4e73c7184cb0f59b8e8610566e1
Ulrich Michael <ulrich1999@web.de>
2022-04-18 23:50:22 +0200

    fixed GET request path

--- backend/advertisementService/src/main/java/service/advertisementservice/TestController.java
+++ backend/advertisementService/src/main/java/service/advertisementservice/TestController.java
@@ -2,12 +2,10 @@ package service.advertisementservice;
 
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/advertisement")
 public class TestController {
     //Returns index.html
 
