8bbcd19399ed88304a41e8ac625ac05558994904
Ulrich Michael <ulrich1999@web.de>
2022-04-27 00:51:11 +0200

    #23: Finished database integration

--- backend/advertisementService/src/main/java/service/advertisementservice/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package service.advertisementservice;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student,String> {
-
-}
