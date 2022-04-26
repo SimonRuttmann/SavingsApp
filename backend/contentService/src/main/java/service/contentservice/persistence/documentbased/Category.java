414b58cc2638bcc9929389ada5262734ae17b567
simon.ruttmann <simon.ruttmann@freenet.de>
2022-04-26 03:02:31 +0200

    feature(Contentservice/persistence/documentbased): Implemented Category document

--- backend/contentService/src/main/java/service/contentservice/persistence/documentbased/Category.java
+++ backend/contentService/src/main/java/service/contentservice/persistence/documentbased/Category.java
@@ -1,8 +1,38 @@
 package service.contentservice.persistence.documentbased;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.util.Objects;
+
+@Document()
 public class Category {
 
-    String id;
-    String name;
+    @Id
+    @Field(targetType = FieldType.OBJECT_ID)
+    public String id;
 
+
+    @Field(targetType = FieldType.STRING)
+    public String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
