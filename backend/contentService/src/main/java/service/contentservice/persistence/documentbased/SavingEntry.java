4207264025224a0578c458de89598e2fcc3f5702
simon.ruttmann <simon.ruttmann@freenet.de>
2022-04-26 03:05:23 +0200

    feature(Contentservice/persistence/documentbased): Implemented Saving entry document

--- backend/contentService/src/main/java/service/contentservice/persistence/documentbased/SavingEntry.java
+++ backend/contentService/src/main/java/service/contentservice/persistence/documentbased/SavingEntry.java
@@ -1,24 +1,66 @@
 package service.contentservice.persistence.documentbased;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.util.Date;
+import java.util.Objects;
 
+@Document()
 public class SavingEntry {
 
     @Id
-    Long savingId;
+    @Field(targetType = FieldType.INT64)
+    public Long savingId;
 
-    String name;
 
-    Double costBalance;
+    @Field(targetType = FieldType.STRING)
+    public String name;
 
-    Category category;
 
-    Date creationDate;
+    @Field(targetType = FieldType.DOUBLE)
+    public Double costBalance;
 
-    String creator;
 
-    String description;
+    @DBRef
+    public Category category;
 
+
+    @Field(targetType = FieldType.DATE_TIME)
+    public Date creationDate;
+
+
+    @Field(targetType = FieldType.STRING)
+    public String creator;
+
+
+    @Field(targetType = FieldType.STRING)
+    public String description;
+
+    public SavingEntry(
+            String name, Double costBalance, Category category,
+            Date creationDate, String creator, String description) {
+        this.name = name;
+        this.costBalance = costBalance;
+        this.category = category;
+        this.creationDate = creationDate;
+        this.creator = creator;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SavingEntry)) return false;
+        SavingEntry that = (SavingEntry) o;
+        return Objects.equals(savingId, that.savingId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(savingId);
+    }
 }
