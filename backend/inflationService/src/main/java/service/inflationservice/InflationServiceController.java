ac85ac07ee393864c05d57e59e703e81d80062cf
Ulrich Michael <ulrich1999@web.de>
2022-04-27 17:08:22 +0200

    #44 (inflationService): data is now filtered for last entry

--- backend/inflationService/src/main/java/service/inflationservice/InflationServiceController.java
+++ backend/inflationService/src/main/java/service/inflationservice/InflationServiceController.java
@@ -1,5 +1,8 @@
 package service.inflationservice;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class InflationServiceController {
     @Autowired DataJobs dataJobs;
 
+    JSONParser parser = new JSONParser();
     @GetMapping("/inflationrate")
-    public String getInflationRate(){
-        return dataJobs.getData();
+    public String getLastInflationRate(){
+        Object obj = null;
+        try {
+            obj = parser.parse(dataJobs.getData());
+            JSONArray json = (JSONArray)obj;
+            return json.get(0).toString();
+
+        }catch (ParseException e){
+            return "Json could not be parsed";
+        }catch (Exception e){
+            return "Unknown error";
+        }
+
     }
 }
