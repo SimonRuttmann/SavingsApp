1078c8567d913c8f75cff30942b03a21f219d93f
simon.ruttmann <simon.ruttmann@freenet.de>
2022-04-28 00:08:05 +0200

    feature(Contentservice/peristence/documentbased): Added id generation of embedded documents

--- backend/contentService/src/main/java/service/contentservice/persistence/documentbased/ICategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package service.contentservice.persistence.documentbased;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ICategoryRepository extends MongoRepository<GroupDocument, String> {
-}
