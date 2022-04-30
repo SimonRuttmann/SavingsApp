ff0e564954ad454fe56377e83ad1dccb0a37a657
simon.ruttmann <simon.ruttmann@freenet.de>
2022-05-01 00:04:02 +0200

    feature(Contentservice/util): Implemented MapperUtil to map DTO to persistence objects

---
+++ backend/contentService/src/main/java/service/contentservice/util/MapperUtil.java
@@ -0,0 +1,47 @@
+package service.contentservice.util;
+
+import service.contentservice.businessmodel.content.CategoryDTO;
+import service.contentservice.businessmodel.content.SavingEntryDTO;
+import service.contentservice.persistence.documentbased.Category;
+import service.contentservice.persistence.documentbased.SavingEntry;
+
+public class MapperUtil {
+
+    public static Category DTOToCategory(CategoryDTO dto){
+        Category category = new Category();
+        category.setId(DocObjectIdUtil.toObjectId(dto.getId()));
+        category.setName(dto.getName());
+        return category;
+    }
+
+    public static CategoryDTO CategoryToDTO(Category category){
+        CategoryDTO categoryDto =  new CategoryDTO();
+        categoryDto.setId(DocObjectIdUtil.toHexString(category.getId()));
+        categoryDto.setName(category.getName());
+        return categoryDto;
+    }
+
+    public static SavingEntry DTOToSavingEntry(SavingEntryDTO dto){
+        SavingEntry savingEntry = new SavingEntry();
+        savingEntry.setCategory(DTOToCategory(dto.getCategory()));
+        savingEntry.setDescription(dto.getDescription());
+        savingEntry.setCreationDate(dto.getCreationDate());
+        savingEntry.setCreator(dto.getCreator());
+        savingEntry.setId(DocObjectIdUtil.toObjectId(dto.getId()));
+        savingEntry.setCostBalance(dto.getCostBalance());
+        savingEntry.setName(dto.getName());
+        return savingEntry;
+    }
+
+    public static SavingEntryDTO SavingEntryToDTO(SavingEntry savingEntry){
+        SavingEntryDTO savingEntryDto =  new SavingEntryDTO();
+        savingEntryDto.setId(DocObjectIdUtil.toHexString(savingEntry.getId()));
+        savingEntryDto.setCategory(CategoryToDTO(savingEntry.getCategory()));
+        savingEntryDto.setCostBalance(savingEntry.getCostBalance());
+        savingEntryDto.setCreator(savingEntry.getCreator());
+        savingEntryDto.setName(savingEntry.getName());
+        savingEntryDto.setDescription(savingEntry.getDescription());
+        savingEntryDto.setCreationDate(savingEntry.getCreationDate());
+        return savingEntryDto;
+    }
+}
