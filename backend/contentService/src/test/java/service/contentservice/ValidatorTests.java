4b293ce5925a523b6fcaf7733ba0122e6b41e575
simon.ruttmann <simon.ruttmann@freenet.de>
2022-05-01 00:01:16 +0200

    feature(Contentservice/tests/*): Updated tests

--- backend/contentService/src/test/java/service/contentservice/ValidatorTests.java
+++ backend/contentService/src/test/java/service/contentservice/ValidatorTests.java
@@ -3,6 +3,7 @@ package service.contentservice;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import service.contentservice.businessmodel.content.CategoryDTO;
 import service.contentservice.persistence.documentbased.Category;
 import service.contentservice.validation.ValidatorFactory;
 
@@ -10,13 +11,13 @@ public class ValidatorTests {
 
     @Test
     public void testValidators() {
-        var validator = ValidatorFactory.getInstance().getValidator(Category.class);
+        var validator = ValidatorFactory.getInstance().getValidator(CategoryDTO.class);
 
 
-        Category category = new Category();
-        category.name = "abc";
-        Assertions.assertTrue(validator.validate(category, false));
-        Assertions.assertFalse(validator.validate(category, true));
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName("abc");
+        Assertions.assertTrue(validator.validate(categoryDTO, false));
+        Assertions.assertFalse(validator.validate(categoryDTO, true));
 
     }
 
