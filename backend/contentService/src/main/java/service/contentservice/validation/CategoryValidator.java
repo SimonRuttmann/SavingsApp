5c74e2cefb81369bfe65d8f6da905d2bee7436cd
simon.ruttmann <simon.ruttmann@freenet.de>
2022-05-01 00:01:47 +0200

    feature(Contentservice/validation): Updated CategoryValidator

--- backend/contentService/src/main/java/service/contentservice/validation/CategoryValidator.java
+++ backend/contentService/src/main/java/service/contentservice/validation/CategoryValidator.java
@@ -1,20 +1,19 @@
 package service.contentservice.validation;
 
+import service.contentservice.businessmodel.content.CategoryDTO;
 import service.contentservice.persistence.documentbased.Category;
 
-import java.util.List;
-
-public class CategoryValidator implements IValidator<Category>{
+public class CategoryValidator implements IValidator<CategoryDTO>{
 
     @Override
-    public boolean validate(Category toValidate, boolean withId) {
+    public boolean validate(CategoryDTO toValidate, boolean withId) {
 
         if(toValidate == null) return false;
 
-        if(withId && toValidate.id == null) return false;
-        if(!withId && toValidate.id != null) return false;
+        if(withId && toValidate.getId() == null) return false;
+        if(!withId && toValidate.getId() != null) return false;
 
-        if(toValidate.name == null) return false;
-        return !toValidate.name.isBlank();
+        if(toValidate.getId() == null) return false;
+        return !toValidate.getName().isBlank();
     }
 }
