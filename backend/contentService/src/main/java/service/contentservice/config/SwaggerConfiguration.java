823c06c47902965fea634c8f834195e9e2e5202d
ver0 <veronika_scheller@web.de>
2022-05-02 18:19:25 +0200

    #40 (contentService ) implement Keycloak
    -all routes are protected now

---
+++ backend/contentService/src/main/java/service/contentservice/config/SwaggerConfiguration.java
@@ -0,0 +1,39 @@
+package service.contentservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    String keycloakAuthUrl = "http://localhost:8090/auth";
+
+    String realm = "haushaltsapp";
+
+    @Bean
+    public OpenAPI envioronmentOpenApi(){
+        String baseUrl = ""+keycloakAuthUrl+"/realms/"+realm+"/protocol/openid-connect";
+        return new OpenAPI()
+                .schemaRequirement(
+                        "OAuth2 Authorization",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .flows(
+                                        new OAuthFlows()
+                                                .password(
+                                                        new OAuthFlow()
+                                                                .authorizationUrl(baseUrl+"/auth")
+                                                                .tokenUrl(baseUrl+"/token")
+                                                                .refreshUrl(baseUrl+"/token")
+                                                                .scopes(new Scopes())
+                                                )
+                                )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("OAuth2 Authorization")
+                );
+
+    }
+}
