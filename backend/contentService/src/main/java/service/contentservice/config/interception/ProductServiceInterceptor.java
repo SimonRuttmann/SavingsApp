0efb84cc83affcf3233d9cdff30e28d57f099b24
ver0 <veronika_scheller@web.de>
2022-05-18 15:34:42 +0200

    #52 (content Service) add check if request-user is in group

--- backend/contentService/src/main/java/service/contentservice/config/interception/ProductServiceInterceptor.java
+++ backend/contentService/src/main/java/service/contentservice/config/interception/ProductServiceInterceptor.java
@@ -1,8 +1,14 @@
 package service.contentservice.config.interception;
 
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
+import service.contentservice.services.DatabaseService;
+import service.userservice.service.imp.DatabaseService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +18,32 @@ import java.util.UUID;
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
 
+    private final DatabaseService databaseService;
+
+    @Autowired
+    public ProductServiceInterceptor(DatabaseService databaseService) {
+        this.databaseService = databaseService;
+    }
+
 
     @Override
     public boolean preHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object map = request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-        map.
+        // get groupId from request-path
+        Map<String,String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        if(map != null && map.size() != 0) {
+            Long groupId = Long.parseLong(map.get("groupId"), 10);
+
+            KeycloakAuthenticationToken principal = (KeycloakAuthenticationToken) request.getUserPrincipal();
+            UUID userId = UUID.fromString(principal.getPrincipal().toString());
+            Boolean check = databaseService.checkIfPersonIsMember(userId, groupId);
+            if(!check) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User is not member of this group");
+        }
+
 
 
 
         return true;
     }
-
 }
