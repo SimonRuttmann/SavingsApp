85dbcf64d455ffed43d8434425d6293cb0e0101e
Ulrich Michael <ulrich1999@web.de>
2022-05-23 20:52:40 +0200

    Trying to bring modularise AdvertisementService

--- backend/inMemoryDatabaseService/src/module-info.java
+++ backend/inMemoryDatabaseService/src/module-info.java
@@ -2,6 +2,7 @@ module inMemoryDatabaseService {
     requires spring.context;
     requires spring.beans;
     requires spring.data.redis;
+    requires java.annotation;
     exports inMemoryDatabaseService.model;
     exports inMemoryDatabaseService.service;
 
