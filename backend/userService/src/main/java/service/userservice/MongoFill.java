51e048ceeef03c08b79a67386cb48db5cd8298c1
Ulrich Michael <ulrich1999@web.de>
2022-06-01 19:08:36 +0200

    (userservice) updated mongoFill

--- backend/userService/src/main/java/service/userservice/MongoFill.java
+++ backend/userService/src/main/java/service/userservice/MongoFill.java
@@ -3,33 +3,36 @@ package service.userservice;
 import com.mongodb.client.MongoClients;
 import documentDatabaseModule.model.Category;
 import documentDatabaseModule.model.GroupDocument;
-import documentDatabaseModule.model.SavingEntry;
 import documentDatabaseModule.service.GroupDocumentService;
 import dtoAndValidation.dto.content.CategoryDTO;
 import dtoAndValidation.dto.content.SavingEntryDTO;
 import dtoAndValidation.util.MapperUtil;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
-import relationalDatabaseModule.service.DatabaseService;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Drops the mongo database and then generates testentries for a fresh database.
  * Class runs through every time it is created and then dies by garbage collector.
  */
+
+@Slf4j
 @Service
 public class MongoFill {
     private final MongoOperations mongoOps;
 
     private final GroupDocumentService groupDocumentService;
 
-    private final int largestGroupId=10;
+    private static final int LARGEST_GROUP_ID =10;
+
+    private static final Random random = new Random();
 
     String[] categoryArray = {"Miete","Lebensmittel","Restaurant","Hund","Katze","Alterversicherung", "Hausversicherung",
             "Rennrad", "BasketballVerein", "Musik Verein", "Zoo","Fastfood","Zocken","Pokern",
@@ -38,7 +41,7 @@ public class MongoFill {
     ArrayList<Category> categories = new ArrayList<>();
 
     public MongoFill() throws ParseException {
-        System.out.println("Starting mongo test data routine");
+        log.info("Starting mongo test data routine");
 
         mongoOps = new MongoTemplate(MongoClients.create(), "test");
         groupDocumentService = new GroupDocumentService(mongoOps);
@@ -53,7 +56,7 @@ public class MongoFill {
     }
 
     private void addDocuments(){
-        for(int groupId =1; groupId <= largestGroupId ; groupId++) {
+        for(int groupId = 1; groupId <= LARGEST_GROUP_ID; groupId++) {
             var d = new GroupDocument();
             d.groupId = (long) groupId;
             GroupDocument savedGroupDoc = groupDocumentService.createDocument(d);
@@ -68,14 +71,12 @@ public class MongoFill {
     private void fillPersonalGroups() throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-        Random random = new Random();
-
-
         for(int month=1 ; month <= 12; month++) {
             for (int day = 1; day <= 28; day++) {
                 int repetitionAmount = random.nextInt(49)+1;
                 for (int repetitions = 0; repetitions<=repetitionAmount; repetitions++ ) {
-                    Long groupId = (long) random.nextInt(1 + largestGroupId) + 1;
+
+                    Long groupId = (long) random.nextInt(1 + LARGEST_GROUP_ID) + 1;
                     int entrynumber = month * 28 + day; // Arbitrary, only used for string naming
                     Double cost = random.nextDouble() * 1000;
                     String name = "Testentry" + entrynumber;
