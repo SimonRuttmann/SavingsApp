044c3603520385c75ddd32f2de8b8e59f624c986
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-01 22:14:37 +0200

    feature(backend/ContentService): Request inflation rate from inflation service

--- backend/relationalDatabaseService/src/main/java/relationalDatabaseModule/model/KPerson.java
+++ backend/relationalDatabaseService/src/main/java/relationalDatabaseModule/model/KPerson.java
@@ -70,7 +70,6 @@ public class KPerson {
     Set<Invitation> invitations = new HashSet<>();
 
     public String getId() { return id;}
-    //public void setId(UUID id) {this.id = id;}
 
     public String getUsername() {return username;}
     public void setUsername(String username) {this.username = username;}
