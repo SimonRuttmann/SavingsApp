52b55bfa9bde7d4d24e7ebbac76810e2c052308c
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-01 22:01:29 +0200

    feature(backend/inflationService): Cleanup of inflationService

--- backend/inflationService/src/main/java/service/inflationservice/service/InflationServiceService.java
+++ backend/inflationService/src/main/java/service/inflationservice/service/InflationServiceService.java
@@ -1,27 +1,29 @@
 package service.inflationservice.service;
 
+import dtoAndValidation.dto.inflation.InflationDto;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import service.inflationservice.model.InflationDto;
 
-import java.net.http.HttpResponse;
 
 @Slf4j
 @Component
 public class InflationServiceService {
     private String data = null;
 
-    //Scheduled to run once on call and then every 10 days after server startup at 0:00
+
+    /**
+     * Scheduled to run once on call and then every 10 days after server startup at 0:00
+     */
     @Scheduled(cron = "0 0 0 1/10 * ?")
     private void getApiData(){
+
         log.info("Scheduled job is running");
         // Query to calculate inflation price (with real values)
         // https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=jQuery1112038048534897455344_1650559210757&country=germany&start=2012%2F1%2F1&end=2012%2F12%2F1&amount=100&format=true&_=1650559210763
@@ -32,10 +34,10 @@ public class InflationServiceService {
         data = restTemplate.getForObject(uri,String.class);
     }
 
-    public ResponseEntity<InflationDto> getLatestInflationData() throws ParseException {
+    public ResponseEntity<InflationDto> getLatestInflationData() {
         JSONParser parser = new JSONParser();
 
-        Object obj = null;
+        Object obj;
 
         try {
             obj = parser.parse(getAllInflationData());
@@ -43,12 +45,14 @@ public class InflationServiceService {
 
             obj= parser.parse(json.get(0).toString());
             JSONObject jsonObject = (JSONObject) obj;
+
             var dto = new InflationDto();
-            dto.setInflationValueInPercent( Double.parseDouble(jsonObject.get("InflationRateRounded").toString()));
+            dto.setInflationValueInPercent(
+                    Double.parseDouble(jsonObject.get("InflationRateRounded").toString()));
 
             return new ResponseEntity<>(dto, HttpStatus.OK) ;
         }catch (Exception e){
-            log.error("Failed to parse json from api. Exception: "+e.toString());
+            log.error("Failed to parse json from api. Exception: "+ e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -56,6 +60,6 @@ public class InflationServiceService {
 
     private String getAllInflationData() {
         if(data == null) getApiData();
-        return data ;
+        return data;
     }
 }
