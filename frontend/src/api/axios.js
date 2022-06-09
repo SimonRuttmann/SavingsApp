5ca386b64c256a74c652114f177087765c5368eb
Ulrich Michael <ulrich1999@web.de>
2022-06-10 00:55:32 +0200

    WIP added potential axios implementation + lookAndFeel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,3 +1,16 @@
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+
+export const fetchData = async () => {
+    let response;
+
+    try {
+        response = await axios.get(url, config);
+    } catch (e) {
+        // catch error
+        throw new Error(e.message)
+    }
+
+    // if success return value
+    return response?.data ? response?.data : null // or set initial value
+}
 
