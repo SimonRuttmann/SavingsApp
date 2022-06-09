3a9a34eb5cba6e98ef8de8c1d37b5ddb1d22f1c6
Ulrich Michael <ulrich1999@web.de>
2022-06-10 01:54:28 +0200

    Added axios module, renamed classes with uppercase start

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
 
-export const fetchData = async () => {
+export function Get(url, config){
     let response;
 
     try {
-        response = await axios.get(url, config);
+        response = axios.get(url, config);
     } catch (e) {
         // catch error
         throw new Error(e.message)
     }
-
-    // if success return value
-    return response?.data ? response?.data : null // or set initial value
+    return response
 }
 
+export function Post(url,body,config){
+        let response;
+
+        try {
+            response = axios.post(url, body, config);
+        } catch (e) {
+            // catch error
+            throw new Error(e.message)
+        }
+        return response
+}
+
+export function Put(){
+    //todo
+}
+
+export function Delete(){
+    //todo
+}
