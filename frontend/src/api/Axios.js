d1bcdf8494402f0ebda37c857b06c69bd7663c57
Ulrich Michael <ulrich1999@web.de>
2022-06-10 23:42:29 +0200

    changed axios

--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -1,31 +1,49 @@
 import axios from 'axios';
 
-export function Get(url, config){
+export function Get(url, auth){
+    console.log("GET URL: ",url)
+    console.log("Config :",auth)
     let response;
-
-    try {
-        response = axios.get(url, config);
-    } catch (e) {
-        // catch error
-        throw new Error(e.message)
+    if(auth == null){
+        console.log("Running GET without auth")
+        try {
+            response = axios.get(url, null)
+        } catch (e) {
+            // catch error
+            throw new Error(e.message)
+        }
+    }
+    else {
+        console.log("Running GET with auth")
+        response = {
+            method: 'get',
+            url: 'http://localhost:8011/userservice/group',
+            headers: {
+                'Authorization': "Bearer " + auth
+            }
+        }
+        response = axios(response)
     }
     return response
 }
 
 export function Post(url,body,config){
-        let response;
+    let response;
+    console.log("POST ",url)
 
-        try {
-            response = axios.post(url, body, config);
-        } catch (e) {
-            // catch error
-            throw new Error(e.message)
-        }
-        return response
+    try {
+        response = axios.post(url, body, config);
+    } catch (e) {
+        // catch error
+        throw new Error(e.message)
+    }
+
+    return response
 }
 
 export function Put(url,body,config){
     let response;
+    console.log("PUT ",url)
 
     try {
         response = axios.put(url, body, config);
@@ -38,6 +56,7 @@ export function Put(url,body,config){
 
 export function Delete(url,config){
     let response;
+    console.log("Delete ",url)
 
     try {
         response = axios.delete(url, config);
--- frontend/src/api/services/User.js
+++ frontend/src/api/services/User.js
@@ -1,46 +1,46 @@
 import {Delete, Get, Post, Put} from "../Axios";
+import UserService from "../Auth";
 
 const UserServiceBaseUrl = "http://localhost:8011/userservice/";
 const InvitationsBaseUrl = UserServiceBaseUrl + "invitation/";
-const GroupBaseUrl = UserServiceBaseUrl + "group/";
+const GroupBaseUrl = UserServiceBaseUrl + "group";
 const UserBaseUrl = UserServiceBaseUrl + "user/";
 
+const token = ()=> UserService.getToken();
 
 //Invitations
-export function declineInvitation(groupId, header){
-    return Put(InvitationsBaseUrl + "decline/" + groupId,null,header)
+export function declineInvitation(groupId){
+    return Put(InvitationsBaseUrl + "decline/" + groupId,null,token())
 }
-export function acceptInvitation(groupId, header){
-    return Put(InvitationsBaseUrl + "accept/" + groupId, null, header)
+export function acceptInvitation(groupId){
+    return Put(InvitationsBaseUrl + "accept/" + groupId, null, token())
 }
-export function invite(body, header){
-    return Post(InvitationsBaseUrl + "invite", body, header)
+export function invite(body){
+    return Post(InvitationsBaseUrl + "invite", body, token())
 }
-export function receive(header){
-    return Get(InvitationsBaseUrl + "receive",header)
+export function receive(){
+    return Get(InvitationsBaseUrl + "receive",token())
 }
 
 //Groups
-export function register(body, header){
-    return Post(GroupBaseUrl + "register", body, header)
+export function register(body){
+    return Post(GroupBaseUrl + "/" + "register", body, token())
 }
-export function getGroup(header){
-    return Get(GroupBaseUrl,header)
-
+export function getGroup(){
+    return Get(GroupBaseUrl,token())
 }
-export function deleteGroup(groupId, header){
-    return Delete(GroupBaseUrl + groupId, header)
-
+export function deleteGroup(groupId){
+    return Delete(GroupBaseUrl + groupId, token())
 }
-export function leaveGroup(groupId, header){
-    return Delete(GroupBaseUrl + "leave/" + groupId, header)
+export function leaveGroup(groupId){
+    return Delete(GroupBaseUrl + "leave/" + groupId, token())
 }
 
 //Users
-export function getUser(userid, header){
-    return Get(UserBaseUrl + userid, header)
+export function getUser(userid){
+    return Get(UserBaseUrl + userid, token())
 }
-export function getUsers(header){
-    return Get(UserBaseUrl + "usernames", header)
+export function getUsers(){
+    return Get(UserBaseUrl + "usernames", token())
 }
 
