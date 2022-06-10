5c327df5dd70dcdd16711c0ccb563fa02d58ee2c
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-10 14:21:33 +0200

    feature(frontend/index.js): Provide store to react components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux';
-import userReducer from './features/user'
-import advertismentReducer from './features/advertisment'
-import inflationReducer from './features/inflation'
-import messagingReducer from './features/messaging'
+import {Provider} from "react-redux";
+import store from "./reduxStore/Store";
 
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-        advertisment: advertismentReducer,
-        inflation: inflationReducer,
-        messaging: messagingReducer
-    },
-})
 
 ReactDOM.render(
   <React.StrictMode>
