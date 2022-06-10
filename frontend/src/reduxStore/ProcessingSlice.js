6f0917398c4ee6d653d93d62221fb8691dae3412
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-10 18:10:23 +0200

    feature(frontend/ProcessingSlice): Implementation of processing slice

---
+++ frontend/src/reduxStore/ProcessingSlice.js
@@ -0,0 +1,24 @@
+import {createSlice} from '@reduxjs/toolkit'
+import {getAllGroupEntries, getSavingEntriesProcessed} from "../api/services/Content";
+import {AddSavingEntries} from "./SavingEntrySlice";
+
+export const ProcessingSlice = createSlice({
+    name: "processing",
+    initialState: {value: {statistic1: {}, statistic2: {}, statistic3: {}}},
+    reducers: {
+        updateProcessingData: (state, action) => {
+            state.value = action.payload
+        },
+    }
+});
+
+export const {updateProcessingData} = ProcessingSlice.actions
+
+export default ProcessingSlice.reducer
+
+export const selectProcessingStore = (state) => state.processing;
+
+export const fetchProcessingResultsFromServer = (header, groupId, filterInformation) => (dispatch) => {
+    let response = getSavingEntriesProcessed(groupId, filterInformation, header)
+    response.then(response => dispatch(updateProcessingData(response.data)));
+}
