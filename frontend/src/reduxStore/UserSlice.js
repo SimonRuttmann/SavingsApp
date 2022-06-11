ded382b5d2635762d0cec7c1ac5f6be30747aa3c
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-12 01:24:50 +0200

    feature(frontend/userSlice): Implementation fetchUsernames

--- frontend/src/reduxStore/UserSlice.js
+++ frontend/src/reduxStore/UserSlice.js
@@ -1,9 +1,9 @@
 import {createSlice} from '@reduxjs/toolkit'
-import {getUser} from "../api/services/User";
+import {getUser, getUsernames} from "../api/services/User";
 
 export const userSlice = createSlice({
     name: "user",
-    initialState: {token: "", id: "", email: "", username: ""},
+    initialState: {token: "", id: "", email: "", username: "", usernames: []},
     reducers: {
         login: (state, action) => {
             state.token = action.payload; //TODO atm, dispatch(login(token)); if object needed dispatch(login({..., token: token}); and here action.payload.token
@@ -15,17 +15,25 @@ export const userSlice = createSlice({
             state.id = action.payload.id;
             state.email = action.payload.email;
             state.username = action.payload.username;
+        },
+        setOtherUserNames: (state, action) => {
+              state.usernames = [];
+              action.payload.forEach(username => state.usernames.push(username))
         }
     }
 })
 
-export const {login, logout, setUserData} = userSlice.actions
+export const {login, logout, setUserData, setOtherUserNames} = userSlice.actions
 
 export const fetchUserDataFromServer = () => (dispatch) => {
     let response = getUser();
     response.then(response => dispatch(setUserData(response.data)));
 }
 
+export const fetchUserNames = () => (dispatch) => {
+    let response = getUsernames();
+    response.then(response => dispatch(setOtherUserNames(response.data)))
+}
 
 export default userSlice.reducer
 
