4a656c19ddff7c751cd0c349f8d3cdaa00568ecd
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-11 02:03:17 +0200

    feature(frontend/Diagrams): Display diagram 1 correctly

--- frontend/src/components/HomepageSite/Diagrams/Diagram1.js
+++ frontend/src/components/HomepageSite/Diagrams/Diagram1.js
@@ -2,22 +2,22 @@ import {Card} from "react-bootstrap";
 import {Bar} from "react-chartjs-2";
 import React from "react";
 
-export const Diagram1 = ({selectedGroup}) => {
+export const Diagram1 = ({diagramValues}) => {
 
     return (
         <Card className="chartStyle">
             <Bar
                 data={{
                     // Name of the variables on x-axies for each bar
-                    labels: ["1st bar", "2nd bar"],
+                    labels: ["Einnahmen", "Ausgaben", "Balance", "Zukünftige Balance"],
                     datasets: [
                         {
                             // Label for bars
                             label: "total count/value",
                             // Data or value of your each variable
-                            data: [selectedGroup.diagrams.diagram1.firstValue, selectedGroup.diagrams.diagram1.secondValue],
+                            data: [diagramValues.income, -diagramValues.outcome, diagramValues.balance, diagramValues.futureBalance],
                             // Color of each bar
-                            backgroundColor: ["aqua", "green"],
+                            backgroundColor: ["green", "red", "yellow", "orange"],
                             // Border color of each bar
                             borderColor: ["aqua", "green"],
                             borderWidth: 0.5,
