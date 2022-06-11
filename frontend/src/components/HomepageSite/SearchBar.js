214e24f5e895a6301ba54352d7d6cce8068a4310
Ulrich Michael <ulrich1999@web.de>
2022-06-11 20:48:26 +0200

    (Homepage) WIP searchbar progress, added searchbar to homepage and adjusted entrytable

--- frontend/src/components/HomepageSite/SearchBar.js
+++ frontend/src/components/HomepageSite/SearchBar.js
@@ -1,65 +1,57 @@
-import {Button, Card, Col, Form, Row} from "react-bootstrap";
+import {Card, Col, Form, Row} from "react-bootstrap";
 import Select from "react-select";
 import React from "react";
 import makeAnimated from "react-select/animated";
-export const SearchBar = ({setSelectedEntry, selectedEntry, mappedCategories, setSelectedCategories, AddEntry, setShowMore, showMore}) => {
+import {Title} from "chart.js";
+export const SearchBar = ({
+                              mappedCategories,
+                              users, selectedUsers, setSelectedUsers,
+                              selectedFilterCategories, setSelectedFilterCategories,
+                              timeWindow, selectedTimeWindow, setSelectedTimeWindow}) => {
 
     const animatedComponents = makeAnimated();
 
     return (
-        <Card>
+        <Card className="searchBar">
             <Card.Body>
                 <Form>
-                    <Row className="searchBar">
+                    <Row >
                         <Col>
                             <Form.Group>
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" placeholder="Name eintragen" onChange={() => setSelectedEntry()} value={ selectedEntry != null ? selectedEntry.name : null} />
+                                <Form.Label>Zeitraum</Form.Label>
+                                <div className="Select">
+                                    <Select options={timeWindow} components={animatedComponents} defaultValue={selectedTimeWindow}  onChange={(e) => setSelectedTimeWindow(e)}
+                                            />
+                                </div>
                             </Form.Group>
                         </Col>
                         <Col>
                             <Form.Group>
-                                <Form.Label>Kosten</Form.Label>
-                                <Form.Control type="text" placeholder="Kosten eintragen" onChange={() => setSelectedEntry()} value={selectedEntry != null ? selectedEntry.costBalance : null} />
+                                <Form.Label>Datum</Form.Label>
+{/*
+                                <Form.Control type="text" placeholder="Datum" onChange={() => setSelectedEntry()} value={selectedEntry != null ? selectedEntry.timestamp : null}/>
+*/}
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="UserArea">
+                                <Form.Label>Users</Form.Label>
+                                <div className="Multiselect">
+                                    <Select options={users} components={animatedComponents} defaultValue={selectedUsers} onChange={(e) => setSelectedUsers(e)}
+                                            isMulti />
+                                </div>
                             </Form.Group>
                         </Col>
                         <Col>
                             <Form.Group className="CategoryArea">
                                 <Form.Label>Kategorien</Form.Label>
                                 <div className="Multiselect">
-                                    <Select options={mappedCategories} components={animatedComponents} onChange={(e) => setSelectedCategories(e)}
+                                    <Select options={mappedCategories} components={animatedComponents} defaultValue={selectedFilterCategories} onChange={(e) => setSelectedFilterCategories(e)}
                                             isMulti />
                                 </div>
                             </Form.Group>
                         </Col>
-                        <Col className="buttonCol">
-                            <Form.Group className="buttonArea">
-                                <Button onClick={() => AddEntry(selectedEntry)}>Eintrag erstellen</Button>
-                                {!showMore &&  <Button variant="link" onClick={() => setShowMore(true)}>Zeig mehr</Button>}
-                            </Form.Group>
-                        </Col>
                     </Row>
-                    <br/>
-                    { showMore &&
-                        <Row>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Datum</Form.Label>
-                                    <Form.Control type="text" placeholder="Datum eintragen" onChange={() => setSelectedEntry()} value={selectedEntry != null ? selectedEntry.timestamp : null}/>
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Beschreibung</Form.Label>
-                                    <Form.Control as="textarea" rows={3} onChange={() => setSelectedEntry()} />
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group>
-                                    <Button variant="link" onClick={() => setShowMore(false)}>Zeig weniger</Button>
-                                </Form.Group>
-                            </Col>
-                        </Row>}
                 </Form>
             </Card.Body>
         </Card>
