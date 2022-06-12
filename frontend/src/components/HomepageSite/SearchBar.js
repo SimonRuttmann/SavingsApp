dd6d6ccd3a826e806698a97e01be70cee6f63dc0
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-12 06:21:18 +0200

    feature(frontend/*): fetch processing results correctly, added datepicker

--- frontend/src/components/HomepageSite/SearchBar.js
+++ frontend/src/components/HomepageSite/SearchBar.js
@@ -1,16 +1,24 @@
 import {Button, Card, Col, Form, Row} from "react-bootstrap";
 import Select from "react-select";
-import React from "react";
+import React, {useState} from "react";
 import makeAnimated from "react-select/animated";
 import {filterInformationAction} from "./Homepage";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale } from  "react-datepicker";
+import de from 'date-fns/locale/de';
 
 export const SearchBar = ({   mappedCategories,
                               users,
                               currentFilterInformation,
                               dispatchFilterInformation}) => {
 
+    registerLocale('de', de)
+
     const animatedComponents = makeAnimated();
     const timeWindow = [{label : "Day"},{label : "Week"},{label : "Month"},{label : "Year"}]
+
+    const [startDate, setStartDate] = useState(new Date().setFullYear(new Date().getFullYear() - 1));
 
     const mapUsers = () =>{
         return users.map(user =>{ return {label: user.username,value: user.id, ...user}});
@@ -35,10 +43,16 @@ export const SearchBar = ({   mappedCategories,
                         </Col>
                         <Col>
                             <Form.Group>
-                                <Form.Label>Datum</Form.Label>
+                                <Form.Label>Start-Datum</Form.Label>
+                                    <DatePicker selected={startDate}
+                                                onChange={(date) => {
+                                                    setStartDate(date);
+                                                    dispatchFilterInformation({type: filterInformationAction.changeStartDate, payload:date});
+                                                    }}
+                                                locale="de"
+                                                dateFormat="dd/MM/yyyy"
+                                    />
 {/*
-                                <Form.Control type="text" placeholder="Datum" onChange={() => setSelectedEntry()} value={selectedEntry != null ? selectedEntry.timestamp : null}/>
-                                //TODO onChange={(e) => dispatchFilterInformation({type: filterInformationAction.changeStartDate, payload:e})}
                                 //TODO onChange={(e) => dispatchFilterInformation({type: filterInformationAction.changeEndDate, payload:e})}
 */}
                             </Form.Group>
