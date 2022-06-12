622be4c06457b3ca8ea71316e0500c1e7534e573
ver0 <veronika_scheller@web.de>
2022-06-12 02:57:54 +0200

    (Settings PopUp) add group funktions ar done, invite is missing, Ui is not that perfekt

--- frontend/src/api/services/User.js
+++ frontend/src/api/services/User.js
@@ -3,7 +3,7 @@ import UserService from "../Auth";
 
 const UserServiceBaseUrl = "http://localhost:8011/userservice/";
 const InvitationsBaseUrl = UserServiceBaseUrl + "invitation/";
-const GroupBaseUrl = UserServiceBaseUrl + "group";
+const GroupBaseUrl = UserServiceBaseUrl + "group/";
 const UserBaseUrl = UserServiceBaseUrl + "user/";
 
 const token = ()=> UserService.getToken();
@@ -24,7 +24,7 @@ export function receive(){
 
 //Groups
 export function register(body){
-    return Post(GroupBaseUrl + "/" + "register", body, token())
+    return Post(GroupBaseUrl + "register", body, token())
 }
 export function getGroup(){
     return Get(GroupBaseUrl,token())
