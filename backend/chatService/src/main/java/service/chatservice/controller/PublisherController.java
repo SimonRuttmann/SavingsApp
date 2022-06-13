0550e9e40b94ccc9cfc2c3c3a73ffba5abce79e2
Ulrich Michael <ulrich1999@web.de>
2022-06-13 19:33:55 +0200

    addition to last commit

--- backend/chatService/src/main/java/service/chatservice/controller/PublisherController.java
+++ backend/chatService/src/main/java/service/chatservice/controller/PublisherController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,8 @@ public class PublisherController {
     @Autowired
     private RedisPublisher redisPublisher;
 
-    @PostMapping(path = "chat/message",
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "chat/message", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = {"http://localhost:3000"})
     public void sendMessageByApi(@RequestBody ChatMessagePayload message){
         String topic = message.getTopic();
         redisPublisher.publish(ChannelTopic.of(topic), message);
