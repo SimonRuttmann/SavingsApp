90f1898e5e55e9562248d168c4b8dfe236c0128c
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-13 22:21:51 +0200

    feature(frontend/chat): Implementation of chatSocket.js

--- frontend/src/utils/ChatSocket.js
+++ frontend/src/utils/ChatSocket.js
@@ -1,34 +1,66 @@
-import React, {useEffect, useRef, useState} from 'react'
 import SockJS from 'sockjs-client';
 import {Stomp} from "@stomp/stompjs";
-import {
-    Card,
-} from "react-bootstrap";
-import {useSelector} from "react-redux";
-import {getMessages, postMessage, subTopic} from "../api/services/Chat";
-import {selectUserStore} from "../reduxStore/UserSlice";
-import "../css/chat.scss"
+import {subTopic} from "../api/services/Chat";
 
-    const socket = new SockJS('http://localhost:8014/ws/chat')
-    let topic = null;
-    let stompClient = null;
-    let isInit = false;
+const socket = new SockJS('http://localhost:8014/ws/chat')
+let topic = null;
+let stompClient = null;
+let isInit = false;
+let subscription;
 
-    export const ChatWebSocket = {
-        init,changeTopic,onMessageReceived
-    }
+const init = (initialTopic, onMessage) => {
+    if(isInit || initialTopic === null) return;
+    initializeSocket(initialTopic, onMessage)
 
-    const init = () =>
-    {
-        if(isInit) return;
-        connectToSockJs()
-    }
-
-    
+}
 
 const connectToSockJs = () => {
-    console.log("Trying to connect ",stompClient)
-    stompClient = Stomp.over(socket)
+    return new Promise( (resolve) => {
+        stompClient = Stomp.over(socket)
+        stompClient.connect({}, () => resolve())
+    })
+}
 
-    stompClient.connect({})
+const subscribeToRedis = (topic) => subTopic(topic)
+
+const subscribeWs = (topic, cb) => {
+    subscription = stompClient.subscribe(`/sub/chat/rooms/`+topic, (msg) => cb(JSON.parse(msg.body)))
+}
+
+const disconnect = () => {
+    stompClient.disconnect()
+    isInit = false;
+}
+
+const initializeSocket = (initialTopic, onMessage) => {
+    connectToSockJs().then(() => subscribeWs(initialTopic, onMessage))
+    subscribeToRedis(topic).catch(() => console.log("Subscribe to redis did not work"));
+}
+
+const changeTopic = (newTopic, onMessage) => {
+
+    if(newTopic === topic) return;
+
+    topic = newTopic;
+    subscription
+        .unsubscribe()
+        .then( () =>  subscribeToRedis(newTopic))
+        .then( () => subscribeWs(newTopic, onMessage))
+}
+
+const getTopic = () => topic;
+
+/**
+ * @param init        Parameters: initialTopic, the topic to subscribe to
+ *                                onMessage, callback invoked with the body of the received message
+ * @param changeTopic Parameter: newTopic, changes the new topic
+ * @param disconnect  Disconnects the socket
+ * @param getTopic    Returns the current topic
+ * @type {{init: init, disconnect: disconnect, changeTopic: changeTopic, getTopic: (function(): null)}}
+ */
+export const ChatWebSocket = {
+    init,
+    changeTopic,
+    disconnect,
+    getTopic
 }
