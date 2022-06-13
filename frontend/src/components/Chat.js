02c3c50d22b39a2f7365a04450125842f351f8d5
Ulrich Michael <ulrich1999@web.de>
2022-06-13 20:53:48 +0200

    (Chat) Bugfixing

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import SockJS from 'sockjs-client';
 import {Stomp} from "@stomp/stompjs";
 import {
@@ -15,6 +15,11 @@ import "../css/chat.scss"
 
 
 function Chat( getActiveGroupId ) {
+    const socket = useRef(new SockJS('http://localhost:8014/ws/chat'))
+    const chatRef = useRef()
+    const topic = useRef(null);
+    const stompClient = useRef(null);
+
     const userStore             = useSelector(selectUserStore);
 
     //All saved messages
@@ -22,44 +27,48 @@ function Chat( getActiveGroupId ) {
     //New messages to be send
     const [message, setMessage] = useState();
     //Topic equals selected groupID
-    const [topic, setTopic] = useState();
+
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
 
-    const handleShow = () => {
-        if(getActiveGroupId.getActiveGroupId != null) {
-            setTopic(getActiveGroupId.getActiveGroupId)
-            console.log("Changing topic to ",topic)
-        }
-        else console.log("Its still null")
-        setShow(true);
-    }
-    let stompClient = null;
+    const handleShow = () => setShow(true);
+    useEffect( () => {
 
-    useEffect(()=>{
-        if(getActiveGroupId.getActiveGroupId != null)
-            setTopic(getActiveGroupId.getActiveGroupId)
-        console.log("Chat changed topic to ",topic)
-
+        if(getActiveGroupId.getActiveGroupId === topic.current || getActiveGroupId.getActiveGroupId == null) return;
+        console.log("INIT topic",topic.current)
+        console.log("INIT getActiveGroupID ",getActiveGroupId.getActiveGroupId)
+        topic.current = getActiveGroupId.getActiveGroupId;
         connectToSockJs()
         subscribeToTopic()
         getMessagesForTopic()
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[getActiveGroupId.getActiveGroupId,topic])
 
     const connectToSockJs = () => {
-        if(stompClient != null)disconnect()
-        const socket = new SockJS('http://localhost:8014/ws/chat')
-        stompClient = Stomp.over(socket)
-        stompClient.connect({}, (frame) => {
+        console.log("Trying to connect ",stompClient)
+        if(stompClient.current != null) disconnect()
+        stompClient.current = Stomp.over(socket.current)
+
+        stompClient.current.connect({}, (frame) => {
             //console.log("Connected: ",frame)
             subscribeToGroup()
         })
     }
-
+        if(stompClient !=null && stompClient.current != null)
+            stompClient.current.onDisconnect = async (receipt) => {
+            console.warn("Has been disconnected");
+        };
+        if(stompClient !=null && stompClient.current != null)
+            stompClient.current.onWebSocketClose = async (evt) => {
+            console.warn("Websocket has been closed");
+        };
     //Handles the subscribe to stomp socket
     const subscribeToGroup = () => {
-        stompClient.subscribe(`/sub/chat/rooms/`+topic, (msg) => {
+        let testindex = 1;
+        stompClient.current.subscribe(`/sub/chat/rooms/`+topic.current, (msg) => {
+            console.log("I am run ",testindex++)
             const json =JSON.parse(msg.body)
             console.log("Message State before: ",messages)
             console.log("Received message: ",json)
@@ -69,6 +78,7 @@ function Chat( getActiveGroupId ) {
     }
 
     const addMessageToState = (_messages) =>{
+        console.log("Wants to add following messages to state ",_messages)
         setMessages(prevState => {
             return [
                 ...prevState,
@@ -79,25 +89,25 @@ function Chat( getActiveGroupId ) {
 
     //Tells the backend to establish a connection to redis sub/pub
     const subscribeToTopic = () => {
-        subTopic(topic)
+        subTopic(topic.current)
     }
 
     const getMessagesForTopic = () => {
-        console.log("Started fetching with topic: ",topic)
-        getMessages(topic).then((tmp)=> {
+        console.log("Started fetching with topic: ",topic.current)
+        getMessages(topic.current).then((tmp)=> {
             setMessages(tmp.data)
         })
     }
 
     const disconnect = () => {
-        stompClient.disconnect()
+        stompClient.current.disconnect()
     }
 
     function sendMessage(){
         const body = {
-            content: message,
+            content: chatRef.current.value,
             sender : userStore.username,
-            topic : topic
+            topic : topic.current
         }
         console.log("Message to send ",body)
         postMessage(body)
@@ -105,7 +115,7 @@ function Chat( getActiveGroupId ) {
 
     function generateShownMessages(){
         let messageForRender = []
-        console.log("Messages to be shown ",messages)
+        //console.log("Messages to be shown ",messages)
         let index = 0;
         for (let msg of messages){
             if(msg.sender === userStore.username){
@@ -142,6 +152,7 @@ function Chat( getActiveGroupId ) {
               </Offcanvas.Body>
               <InputGroup className="mb-3">
                   <FormControl
+                      ref={chatRef}
                       value={message} onChange={(e) => {setMessage(e.target.value)}}
                       placeholder="Gebe deine Nachricht ein"
                       aria-label="message"
