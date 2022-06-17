c8cbc6d8242779f2a25bfcbcdce3918ef855cb9b
ver0 <veronika_scheller@web.de>
2022-06-17 19:03:36 +0200

    (AdverisementServiceController) add Mapping for /homepage

--- backend/advertisementService/src/main/java/service/advertisementservice/controller/AdvertisementServiceController.java
+++ backend/advertisementService/src/main/java/service/advertisementservice/controller/AdvertisementServiceController.java
@@ -2,6 +2,7 @@ package service.advertisementservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import service.advertisementservice.model.AdvertisementDto;
@@ -18,6 +19,17 @@ public class AdvertisementServiceController {
      */
     @GetMapping("/")
     public ModelAndView index(){
+        try {
+            ModelAndView modelAndView= new ModelAndView();
+            modelAndView.setViewName("index");
+            return modelAndView;
+        } catch (Exception e) {
+            System.out.println("execption"+ e.getMessage());
+        }
+        return null;
+    }
+    @GetMapping("/homepage")
+    public ModelAndView indexHome(){
         try {
             ModelAndView modelAndView= new ModelAndView();
             modelAndView.setViewName("index");
